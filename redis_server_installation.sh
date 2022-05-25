116c7a8f12908513cfe81d133ddf89a82a73ab45
Damián Maleno <61498645+Damian-MG@users.noreply.github.com>
2022-05-25 18:38:55 +0200

    Update redis_server_installation.sh

--- redis_server_installation.sh
+++ redis_server_installation.sh
@@ -37,7 +37,7 @@ lithops test
 apt install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
 
 python3 -m pip install boto3
 
