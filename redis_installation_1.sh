02602631c771cbf711bd918fc386ccc0260d5f91
Damian-MG <malenogon_f@hotmail.com>
2022-05-25 11:52:20 +0200

    new script

--- redis_installation_1.sh
+++ redis_installation_1.sh
@@ -40,7 +40,66 @@ sudo ./aws/install
 
 python3 -m pip install boto3
 
-echo "PLEASE RESTART THE VIRTUAL MACHINE AND EXECUTE redis_installation_2.sh"
+# 4. Test the installation of lithops
+lithops test
+
+# 5. Create lithops config file
+echo "
+lithops:
+    backend: aws_lambda
+    storage: aws_s3
+    #data_cleaner: <True/False>
+    #monitoring: storage
+    #monitoring_interval: 2
+    data_limit: False  # in MiB
+    #execution_timeout: 1800
+    #include_modules: <LIST_OF_MODULES>
+    #exclude_modules: <LIST_OF_MODULES>
+    #log_level: INFO
+    #log_format: '%(asctime)s [%(levelname)s] %(name)s -- %(message)s'
+    #log_stream: ext://sys.stdout
+    #log_filename <path_to_a_file>
+    #customized_runtime: <True/False>
+
+aws:
+    access_key_id:
+    secret_access_key:
+    account_id:
+
+aws_s3:
+    storage_bucket:
+    region_name:
+
+aws_lambda:
+    execution_role:
+    region_name:
+
+redis:
+    host: localhost
+    port: 6379
+    password: ${password}
+" > .lithops/config
+
+# 6. User task
+echo "
+PLEASE ACCESS LITHOPS CONFIG FILE AND FILL THE EMPTY FIELDS
+In the command prompt:
+cd .lithops/
+vi config
+(:wq to save and exit)
+
+" 
+
+echo "
+PLEASE CONFIGURE NOW THE AWS CLIENT
+In the command prompt:
+aws configure
+(fill the corresponding fields)
+"
+
+echo"
+END
+"
 # END
 
 
