a983ad4169e65c61af1dae954e2fd14bfbdfe4ab
Damián Maleno <61498645+Damian-MG@users.noreply.github.com>
2022-05-27 13:47:29 +0200

    Update redis_server_installation_2.sh

--- redis_server_installation_2.sh
+++ redis_server_installation_2.sh
@@ -9,13 +9,13 @@ password=$1
 # Superuser check
 if [ $(id -u) != "0" ]; then
 	# 1. Lithops installation and test
-	python3 -m pip install lithops
+	pip3 install lithops
 	lithops test
 
 	# 2. AWS dependencies (aws-cli and boto3)
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
-	./aws/install
+	sudo ./aws/install
 
 	#python3 -m pip install boto3
 
@@ -24,39 +24,39 @@ if [ $(id -u) != "0" ]; then
 	mkdir .lithops/
 
 	# 4. Create Lithops config file
-	echo "lithops:
-	    backend: aws_lambda
-	    storage: aws_s3
-	    #data_cleaner: <True/False>
-	    #monitoring: storage
-	    #monitoring_interval: 2
-	    data_limit: False  # in MiB
-	    #execution_timeout: 1800
-	    #include_modules: <LIST_OF_MODULES>
-	    #exclude_modules: <LIST_OF_MODULES>
-	    #log_level: INFO
-	    #log_format: '%(asctime)s [%(levelname)s] %(name)s -- %(message)s'
-	    #log_stream: ext://sys.stdout
-	    #log_filename <path_to_a_file>
-	    #customized_runtime: <True/False>
+	echo "lithops:	
+    backend: aws_lambda
+    storage: aws_s3
+    #data_cleaner: <True/False>
+    #monitoring: storage
+    #monitoring_interval: 2
+    data_limit: False  # in MiB
+    #execution_timeout: 1800
+    #include_modules: <LIST_OF_MODULES>
+    #exclude_modules: <LIST_OF_MODULES>
+    #log_level: INFO
+    #log_format: '%(asctime)s [%(levelname)s] %(name)s -- %(message)s'
+    #log_stream: ext://sys.stdout
+    #log_filename <path_to_a_file>
+    #customized_runtime: <True/False>
 
-	aws:
-	    access_key_id:
-	    secret_access_key:
-	    account_id:
+aws:
+    access_key_id:
+    secret_access_key:
+    account_id:
 
-	aws_s3:
-	    storage_bucket:
-	    region_name:
+aws_s3:
+    storage_bucket:
+    region_name:
 
-	aws_lambda:
-	    execution_role:
-	    region_name:
+aws_lambda:
+    execution_role:
+    region_name:
 
-	redis:
-	    host: localhost
-	    port: 6379
-	    password: ${password}" > .lithops/config
+redis:
+    host: localhost
+    port: 6379
+    password: ${password}" > .lithops/config
 
 	 # 5. User tasks
 	echo "
